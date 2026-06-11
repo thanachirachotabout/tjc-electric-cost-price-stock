03647fc513c0179859d1ef040e5720bf7e3d1e1b
thanachirachotabout <thanachirachot.about@gmail.com>
2026-06-11 16:17:42 +0700

    Update cloud-config.js

--- src/cloud-config.js
+++ src/cloud-config.js
@@ -1,5 +1,5 @@
 window.TJC_ELECTRIC_CLOUD_CONFIG = {
-  enabled: false,
+  enabled: true,
   supabaseUrl: "https://pofwbdwazfilvrmlhmiw.supabase.co",
   anonKey: "sb_publishable_qJ1NvF_9Ej0FhMlMezL7uA_513R4sb_",
   workspaceId: "tjc-electric-main"
